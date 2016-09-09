c6cb28fdb6c3ee511bb677009c2a08338a2cdf79
gzt0605 <zhongting.ge@ssfs.org>
2016-09-09 14:14:47 -0400

    Final commit

---
+++ Octagon.py
@@ -0,0 +1,54 @@
+# Assignment One_Octagon
+# Ryan Ge
+# Sep 6, 2016
+
+import turtle
+def drawoctagon():
+    for x in range(8):
+        turtle.forward(100)
+        turtle.left(45)
+
+turtle.speed(500)
+def goto(x,y):
+    turtle.penup()
+    turtle.goto(x,y)
+    turtle.pendown()
+
+turtle.color('blue')
+turtle.begin_fill()
+
+turtle.end_fill()
+
+turtle.penup()
+turtle.left(180)
+turtle.forward(180)
+turtle.pendown()
+
+turtle.color('Gold')
+turtle.begin_fill()
+goto(100,-50)
+drawoctagon()
+turtle.end_fill()
+
+turtle.penup()
+turtle.forward(180)
+turtle.right(180)
+turtle.pendown()
+
+turtle.color('Brown')
+turtle.begin_fill()
+turtle.penup()
+turtle.goto(-300,0)
+turtle.pendown()
+drawoctagon()
+turtle.end_fill()
+
+turtle.penup()
+turtle.goto(-300,-290)
+turtle.pendown()
+turtle.color('Grey')
+turtle.begin_fill()
+drawoctagon()
+turtle.end_fill()
+
+turtle.exitonclick()
